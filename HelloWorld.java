56c6b991d295eeda79ab09ccbd835d7dc862ea21
Rajesh Kumar <rajeshgunit@outlook.com>
2022-08-09 18:13:06 +0530

    variable add

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-System.out.println("Rajesh");
-System.out.println("Kumar");
+System.out.println("magesh");
+System.out.println("mani");
   }
 }
